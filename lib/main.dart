31be82f9557e66410404dae9efb5218e7235ee03
AhmedGamal740 <ahmedgamal719074@gmail.com>
2022-04-07 22:04:13 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:todo/shared/cubit/bloc_observer.dart';
+
+import 'layout/home_layout.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeLayout(),
+    );
+  }
+}
+
+
